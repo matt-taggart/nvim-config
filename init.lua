eddab6aa6efd20b6bce29653ecf759c5df4ad4e9
Matt Taggart <mtaggart@wellsuited.com>
2022-10-15 16:19:24 -0700

    feat: update config

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 local set = vim.opt
 local api = vim.api
 
-vim.g.mapleader = " "
-vim.g.js_debug_font_color = "cyan"
+vim.g.mapleader = ' '
+vim.g.js_debug_font_color = 'cyan'
 vim.g.catppuccin_flavour = "mocha"
 
 set.tabstop = 2
@@ -12,10 +12,18 @@ set.expandtab = true
 set.number = true
 set.relativenumber = true
 set.hlsearch = true
-set.clipboard = "unnamedplus"
+set.clipboard = 'unnamedplus'
 
 vim.cmd [[
   colorscheme tokyonight
+  let g:test#javascript#runner = "jest"
+  let g:test#javascript#jest#file_pattern = '.*'
+  let g:test#javascript#jest#executable = 'yarn test'
+  let test#strategy = "neovim"
+  let g:test#neovim#start_normal = 1 
+  let g:user_emmet_install_global=0
+  let g:UltiSnipsSnippetDirectories = [$HOME.'/.vim/UltiSnips']
+  autocmd FileType html,css,javascript,javascriptreact,typescript,typescriptreact EmmetInstall
 ]]
 
 local highlightGroup = api.nvim_create_augroup("highlight_yank", { clear = true })
@@ -24,32 +32,35 @@ api.nvim_create_autocmd(
   { pattern = "*", command = "silent! lua vim.highlight.on_yank({higroup=\"IncSearch\", timeout=300})", group = highlightGroup }
 )
 
-vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("n", "<Leader>w", ":w<CR>")
-vim.keymap.set("n", "<Leader>q", ":q<CR>")
-vim.keymap.set("n", "<Leader>f", ":lua require\"telescope.builtin\".find_files{}<CR>")
-vim.keymap.set("n", "<Leader>/", ":lua require\"telescope.builtin\".live_grep{}<CR>")
-vim.keymap.set("n", "<Leader>s", ":lua require\"telescope.builtin\".grep_string{}<CR>")
-vim.keymap.set("n", "<Leader>b", ":lua require\"telescope.builtin\".buffers{}<CR>")
-vim.keymap.set("n", "<C-q>", ":lua require\"telescope.builtin\".quickfix{}<CR>")
-vim.keymap.set("n", "<Leader>g", ":Git<CR>")
-vim.keymap.set("n", "<Leader>q", ":q<CR>")
-vim.keymap.set("n", "<Leader>c", ":bd<CR>")
-vim.keymap.set("n", "<Leader>C", ":BufOnly<CR>")
-vim.keymap.set("n", "<Leader>v", ":e ~/.config/nvim/init.lua<CR>")
-vim.keymap.set("n", "<Leader>r", ":ReplaceInBuffer<CR>")
-vim.keymap.set("i", "{", "{}<Esc>i")
-vim.keymap.set("i", "(", "()<Esc>i")
-vim.keymap.set("n", "<Leader>t", ":TestNearest<CR>")
-vim.keymap.set("n", "<Leader>T", ":TestFile<CR>")
-vim.keymap.set("n", "<Leader>a", ":TestSuite<CR>")
-
+vim.keymap.set('i', 'jk', '<Esc>')
+vim.keymap.set('n', '<Leader>w', ':w<CR>')
+vim.keymap.set('n', '<Leader>q', ':q<CR>')
+vim.keymap.set('n', '<Leader>f', ':lua require\'telescope.builtin\'.find_files{}<CR>')
+vim.keymap.set('n', '<Leader>/', ':lua require\'telescope.builtin\'.live_grep{}<CR>')
+vim.keymap.set('n', '<Leader>s', ':lua require\'telescope.builtin\'.grep_string{}<CR>')
+vim.keymap.set('n', '<Leader>b', ':lua require\'telescope.builtin\'.buffers{}<CR>')
+vim.keymap.set('n', '<C-q>', ':lua require\'telescope.builtin\'.quickfix{}<CR>')
+vim.keymap.set('n', '<Leader>g', ':Git<CR>')
+vim.keymap.set('n', '<Leader>m', ':Merginal<CR>')
+vim.keymap.set('n', '<Leader>q', ':q<CR>')
+vim.keymap.set('n', '<Leader>c', ':bd<CR>')
+vim.keymap.set('n', '<Leader>C', ':BufOnly<CR>')
+vim.keymap.set('n', '<Leader>v', ':e ~/.config/nvim/init.lua<CR>')
+vim.keymap.set('n', '<Leader>r', ':ReplaceInBuffer<CR>')
+vim.keymap.set('i', '{', '{}<Esc>i')
+vim.keymap.set('i', '(', '()<Esc>i')
+vim.keymap.set('i', '[', '[]<Esc>i')
+vim.keymap.set('i', '\'', '\'\'<Esc>i')
+vim.keymap.set('i', '"', '""<Esc>i')
+vim.keymap.set('n', '<Leader>t', ':TestNearest<CR>')
+vim.keymap.set('n', '<Leader>T', ':TestFile<CR>')
+vim.keymap.set('n', '<Leader>a', ':TestSuite<CR>')
 
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
     vim.cmd [[packadd packer.nvim]]
     return true
   end
@@ -59,17 +70,17 @@ end
 local packer_bootstrap = ensure_packer()
 
 
-require("packer").startup(function(use)
-  use { "wbthomason/packer.nvim", opt = true }
+require('packer').startup(function(use)
+  use { 'wbthomason/packer.nvim', opt = true }
   use {
     "williamboman/mason.nvim",
-    config = function() require("mason").setup() end
+    config = function() require('mason').setup() end
   }
   use {
     "williamboman/mason-lspconfig.nvim",
-    config = function() require("mason-lspconfig").setup() end
+    config = function() require('mason-lspconfig').setup() end
   }
-  use "mfussenegger/nvim-dap"
+  use 'mfussenegger/nvim-dap'
   use "nvim-lua/plenary.nvim"
   use {
     "jose-elias-alvarez/null-ls.nvim",
@@ -88,58 +99,55 @@ require("packer").startup(function(use)
   use "EdenEast/nightfox.nvim"
   use "folke/tokyonight.nvim"
   use {
-    "nvim-treesitter/nvim-treesitter",
-    run = function() require("nvim-treesitter.install").update({ with_sync = true }) end,
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
   }
-  use "kyazdani42/nvim-web-devicons"
-  use "tpope/vim-commentary"          
-  use "tpope/vim-fugitive"          
-  use "tpope/vim-surround"
-  use "tpope/vim-repeat"
-  use "tpope/vim-vinegar"
-  use "michaeljsmith/vim-indent-object"
-  use "kana/vim-textobj-user"
-  use "whatyouhide/vim-textobj-xmlattr"
-  use "christoomey/vim-tmux-navigator"
-  use "blindFS/vim-taskwarrior"
-  use "idanarye/vim-merginal"
-  use "tpope/vim-unimpaired"
-  use "vim-scripts/ReplaceWithRegister"
-  use "nvim-telescope/telescope.nvim"
-  use "vim-scripts/BufOnly.vim"
-  use "ggandor/lightspeed.nvim"
-  use "kevinhwang91/nvim-bqf"
-  use "prettier/vim-prettier"
-  use "sirver/ultisnips"
-  use "vim-test/vim-test"
+  use 'kyazdani42/nvim-web-devicons' 
+  use 'tpope/vim-commentary'           
+  use 'tpope/vim-fugitive'           
+  use 'tpope/vim-surround'
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-vinegar'
+  use 'michaeljsmith/vim-indent-object'
+  use 'kana/vim-textobj-user'
+  use 'whatyouhide/vim-textobj-xmlattr'
+  use 'christoomey/vim-tmux-navigator'
+  use 'blindFS/vim-taskwarrior'
+  use 'idanarye/vim-merginal'
+  use 'tpope/vim-unimpaired'
+  use 'vim-scripts/ReplaceWithRegister' 
+  use 'nvim-telescope/telescope.nvim'
+  use 'vim-scripts/BufOnly.vim'
+  use 'ggandor/lightspeed.nvim'
+  use 'kevinhwang91/nvim-bqf'
+  use 'prettier/vim-prettier'
+  use 'sirver/ultisnips'
+  use 'vim-test/vim-test'
   use {
-    "feline-nvim/feline.nvim",
-    config = function() require("feline").setup() end
-  }
-  use {
-    "lewis6991/gitsigns.nvim",
-    config = function() require("gitsigns").setup() end
-  }
-  use "matt-taggart/vim-js-debug"
-  use "matt-taggart/replace-in-buffer"
-  use {
-    "wellle/targets.vim",
-    event = "VimEnter"
+    'feline-nvim/feline.nvim',
+    config = function() require('feline').setup() end
   }
   use {
-    "psliwka/vim-smoothie",
-    event = "VimEnter",
+    'lewis6991/gitsigns.nvim',
+    config = function() require('gitsigns').setup() end
   }
- 
+  use 'matt-taggart/vim-js-debug'
+  -- use 'matt-taggart/replace-in-buffer'
   use {
-    "folke/zen-mode.nvim",
-    cmd = "ZenMode"
+    'wellle/targets.vim',
+    event = 'VimEnter'
+  }
+  use { 'psliwka/vim-smoothie' }
+  
+  use {
+    'folke/zen-mode.nvim',
+    cmd = 'ZenMode'
   }
 
   use {
-    "catppuccin/nvim",
-    as = "catppuccin",
-    config = function() require("catppuccin").setup() end
+    'catppuccin/nvim',
+    as = 'catppuccin',
+    config = function() require('catppuccin').setup() end
   }
 
   use({"onsails/lspkind-nvim", event = "VimEnter"})
@@ -147,7 +155,7 @@ require("packer").startup(function(use)
   use {
     "hrsh7th/nvim-cmp",
     after = "lspkind-nvim",
-    config = function()
+    config = function() 
       require "nvim-cmp"
     end
   }
@@ -158,11 +166,13 @@ require("packer").startup(function(use)
   use { "hrsh7th/cmp-path", after = "nvim-cmp" }
   use { "hrsh7th/cmp-buffer", after = "nvim-cmp" }
   use { "hrsh7th/cmp-omni", after = "nvim-cmp" }
+  use { "mattn/emmet-vim" }
+  use { "lewis6991/impatient.nvim" }
 
   -- use {"hrsh7th/cmp-cmdline", after = "nvim-cmp"}
-  use {"quangnguyen30192/cmp-nvim-ultisnips", after = {"nvim-cmp", "ultisnips"}}
+  use {"quangnguyen30192/cmp-nvim-ultisnips", after = {'nvim-cmp', 'ultisnips'}}
   if vim.g.is_mac then
-    use {"hrsh7th/cmp-emoji", after = "nvim-cmp"}
+    use {"hrsh7th/cmp-emoji", after = 'nvim-cmp'}
   end
 
   -- nvim-lsp configuration (it relies on cmp-nvim-lsp, so it should be loaded after cmp-nvim-lsp).
@@ -178,7 +188,8 @@ require("packer").startup(function(use)
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
-    require("packer").sync()
+    require('packer').sync()
   end
 end)
+
 
--- lua/nvim-cmp.lua
+++ lua/nvim-cmp.lua
@@ -1,5 +1,5 @@
 -- Set up nvim-cmp.
-local cmp = require"cmp"
+local cmp = require'cmp'
 
 cmp.setup({
   snippet = {
@@ -13,43 +13,43 @@ cmp.setup({
     -- documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "ultisnips" }, -- For ultisnips users.
+    { name = 'nvim_lsp' },
+    { name = 'ultisnips' }, -- For ultisnips users.
   }, {
-    { name = "buffer" },
+    { name = 'buffer' },
   })
 })
 
 -- Set configuration for specific filetype.
-cmp.setup.filetype("gitcommit", {
+cmp.setup.filetype('gitcommit', {
   sources = cmp.config.sources({
-    { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
   }, {
-    { name = "buffer" },
+    { name = 'buffer' },
   })
 })
 
--- Use buffer source for `/` (if you enabled `native_menu`, this won"t work anymore).
-cmp.setup.cmdline("/", {
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
-    { name = "buffer" }
+    { name = 'buffer' }
   }
 })
 
--- Use cmdline & path source for ":" (if you enabled `native_menu`, this won"t work anymore).
-cmp.setup.cmdline(":", {
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
-    { name = "path" }
+    { name = 'path' }
   }, {
-    { name = "cmdline" }
+    { name = 'cmdline' }
   })
 })
